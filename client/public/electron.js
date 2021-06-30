afd536c20de81fffca9aed05464b8faa397c9f88
Johnny Oshika <johnnyoshika@gmail.com>
2021-06-29 21:11:16 -0700

    Log autoUpdater status + bump version

--- client/public/electron.js
+++ client/public/electron.js
@@ -43,9 +43,11 @@ ipcMain.handle(
   },
 );
 
+let win;
+
 function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1600,
     height: 880,
     webPreferences: {
@@ -91,6 +93,40 @@ app.on('activate', () => {
   }
 });
 
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', info => {
+  sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', info => {
+  sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', err => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', progressObj => {
+  let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+  log_message =
+    log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message =
+    log_message +
+    ' (' +
+    progressObj.transferred +
+    '/' +
+    progressObj.total +
+    ')';
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on('update-downloaded', info => {
+  sendStatusToWindow('Update downloaded');
+});
+
 app.on('ready', function () {
   autoUpdater.checkForUpdatesAndNotify();
- });
+});
